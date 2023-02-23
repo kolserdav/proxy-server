0e402d02681ee936eba660cc4574a8542d9c99af
kolserdav <serega12101983@gmail.com>
2023-02-24 02:10:54 +0700

    add test

--- src/http.rs
+++ src/http.rs
@@ -1,24 +1,54 @@
+use std::{
+    io::{Read, Result, Write},
+    net::TcpStream,
+};
+
 pub struct Http {
-    r: char,
-    n: char,
+    socket: TcpStream,
 }
 
 impl Http {
-    pub fn new() -> Http {
-        Http { r: '\r', n: '\n' }
+    pub fn connect(address: &str) -> Result<Http> {
+        let mut socket = TcpStream::connect(address)?;
+        Ok(Http { socket })
     }
-    fn serialize(&self, data: &str) -> String {
-        format!(
-            "{}{}{}{}{}{}",
-            data.len(),
-            self.r,
-            self.n,
-            data,
-            self.r,
-            self.n
-        )
+}
+
+impl Read for Http {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.socket.read(buf)
     }
-    pub fn chunk(&self, data: &str) -> String {
-        self.serialize(data)
+
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize> {
+        let mut size = 0;
+        loop {
+            let mut b = [0; 1];
+            let len = self.read(&mut b)?;
+            if len == 0 {
+                break;
+            }
+            let b = b[0];
+            let len = buf.len();
+            size += 1;
+            if len > 2
+                && b == 10
+                && (buf[len - 1] == 10 || (buf[len - 1] == 13 && buf[len - 2] == 10))
+            {
+                buf.push(b);
+                break;
+            }
+            buf.push(b);
+        }
+        Ok(size)
+    }
+}
+
+impl Write for Http {
+    fn write(&mut self, data: &[u8]) -> Result<usize> {
+        self.socket.write(data)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.socket.flush()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,38 @@ use std::{
     io::{ErrorKind, Read, Result, Write},
     net::{TcpListener, TcpStream},
     str,
-    thread::{sleep, spawn},
-    time::Duration,
 };
 mod thread_pool;
 use thread_pool::ThreadPool;
-mod headers;
-use headers::Headers;
 mod http;
 use http::Http;
 mod log;
 use log::{Log, LogLevel};
 
+#[cfg(test)]
+use std::{
+    thread::{sleep, spawn},
+    time::Duration,
+};
+
 const CHUNK_SIZE: usize = 1024;
+
+#[cfg(test)]
+const ECHO: [char; 4] = ['e', 'c', 'h', 'o'];
+
+static TARGET_ADDRESS: &str = "127.0.0.1:3001";
+static THREADS: usize = 4;
 static LOG_LEVEL: LogLevel = LogLevel::Info;
+static PROXY_ADDRESS: &str = "127.0.0.1:3000";
 
 fn handle_proxy(client: &mut TcpStream) -> Result<()> {
     let _log = Log::new(&LOG_LEVEL);
 
     _log.println(LogLevel::Info, "handle proxy", &client);
-
-    let mut stream = TcpStream::connect("127.0.0.1:3001")?;
-    stream.write("GET / HTTP.1.1\r\nHost: 127.0.0.1:3001\r\n\r\n".as_bytes())?;
-    let mut heads = Headers::new(&mut stream);
+    let mut http = Http::connect(TARGET_ADDRESS)?;
+    http.write("GET / HTTP.1.1\r\nHost: 127.0.0.1:3001\r\n\r\n".as_bytes())?;
     let mut h = vec![];
-    heads.read_to_end(&mut h)?;
+    http.read_to_end(&mut h)?;
 
     _log.println(
         LogLevel::Info,
@@ -37,7 +44,7 @@ fn handle_proxy(client: &mut TcpStream) -> Result<()> {
 
     loop {
         let mut b = [0; CHUNK_SIZE];
-        let r_res = stream.read(&mut b);
+        let r_res = http.read(&mut b);
         if let Err(e) = r_res {
             let log_l = match e.kind() {
                 ErrorKind::ConnectionReset => LogLevel::Info,
@@ -56,7 +63,7 @@ fn handle_proxy(client: &mut TcpStream) -> Result<()> {
         if buf.len() == 0 {
             break;
         }
-        client.write(&buf).expect("failed send chunk from thread");
+        client.write(&buf).expect("Failed write chunk");
     }
     Ok(())
 }
@@ -64,26 +71,57 @@ fn handle_proxy(client: &mut TcpStream) -> Result<()> {
 fn proxy(addr: &str) -> Result<()> {
     let listener = TcpListener::bind(addr)?;
     let _log = Log::new(&LOG_LEVEL);
-    println!("listening proxy on {}", addr);
+    println!("Listening proxy on {}", addr);
 
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(THREADS);
 
     for stream in listener.incoming() {
         pool.execute(|| {
-            handle_proxy(&mut stream.expect("error in stream of tcp listener"))
-                .expect("failed handle proxy");
+            handle_proxy(&mut stream.expect("Error in incoming stream"))
+                .expect("Failed handle proxy");
         });
     }
     Ok(())
 }
 
 fn main() {
-    spawn(move || {
-        target("127.0.0.1:3001").expect("error target");
-    });
-    proxy("127.0.0.1:3000").expect("error proxy");
+    proxy(PROXY_ADDRESS).expect("Error in proxy");
+    // test();
 }
 
+#[test]
+fn test() -> Result<()> {
+    spawn(move || {
+        target(TARGET_ADDRESS).expect("Error in target");
+    });
+    spawn(move || {
+        proxy(PROXY_ADDRESS).expect("Error in proxy");
+    });
+    sleep(Duration::from_secs(1));
+    let mut http = Http::connect(PROXY_ADDRESS)?;
+    let mut buff: Vec<u8> = vec![];
+    http.read_to_end(&mut buff)?;
+    buff = vec![];
+    http.read_to_end(&mut buff)?;
+    let res = str::from_utf8(&buff).unwrap();
+    let sp = res.split("\r\n").filter(|d| !d.is_empty());
+    let mut v = vec![];
+    let mut i = 0;
+    for s in sp {
+        i += 1;
+        if i % 2 == 0 {
+            v.push(s.to_string());
+        }
+    }
+    let mut t_v = vec![];
+    for l in ECHO {
+        t_v.push(l.clone().to_string());
+    }
+    assert_eq!(t_v, v);
+    Ok(())
+}
+
+#[cfg(test)]
 fn target(addr: &str) -> Result<()> {
     let listener = TcpListener::bind(addr)?;
     println!("listening target on {}", addr);
@@ -93,18 +131,19 @@ fn target(addr: &str) -> Result<()> {
     Ok(())
 }
 
+#[cfg(test)]
 fn handle_target(client: &mut TcpStream) -> Result<()> {
     let _log = Log::new(&LOG_LEVEL);
-    const ECHO: [char; 5] = ['e', 'c', 'h', 'o', '\n'];
     _log.println(LogLevel::Info, "handle target", &client);
 
-    let http = Http::new();
     client.write("HTTP/1.1 200 OK\r\nContent-Type: plain/text\r\nTransfer-Encoding: chunked\r\nServer: echo-rs\r\n\r\n".as_bytes()).unwrap();
-    for i in 0..8 {
-        let chunk = http.chunk("e");
+    for i in ECHO {
+        let chunk = format!("1\r\n{}\r\n", i);
         client.write(chunk.as_bytes()).unwrap();
     }
+
     client.write("0\r\n\r\n".as_bytes()).unwrap();
     sleep(Duration::from_millis(100));
+
     Ok(())
 }
